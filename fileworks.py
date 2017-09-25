68ebfe3f194c266019ad2752d65ee469b5e49ce0
IQuant <quant3234@gmail.com>
2017-09-25 15:25:13 +0300

    Some additions to fileworks lib
    -Output of "get_all_files" function is 1d list, not a list for each directory
    -Function "get_all_files" is not inside class now.
    -Testing starts only when lib file is "__main__"

--- fileworks.py
+++ fileworks.py
@@ -3,28 +3,29 @@ import itertools
 import sys
 import concurrent.futures as cf
 
-class fileseeker:
-	def __init__(self):
-		pass
-	def get_all_files(self, path):
-		def files_in_dir(x):
-			try:
-				return list(itertools.filterfalse(lambda x:x.is_dir(), x.iterdir()))
-			except Exception as e:
-				print(e, file=sys.stderr)
+def get_all_files(path):
+	def files_in_dir(x):
+		out = []
 		try:
-			path = pathlib.Path(path)
-			dirs_to_seek = path.rglob("")
-			with cf.ThreadPoolExecutor(max_workers=4) as ex:
-				files = ex.map(files_in_dir, dirs_to_seek)
-			return list(itertools.chain(files))
-		except Exception as e:
-			raise e
-			
+			out = list(itertools.filterfalse(lambda x:x.is_dir(), x.iterdir()))
+		except PermissionError as e:
+			print("[Warn] Access denied to", x, file=sys.stderr)
+		if len(out)>0:
+			return out
+	try:
+		path = pathlib.Path(path)
+		dirs_to_seek = path.rglob("")
+		with cf.ThreadPoolExecutor(max_workers=4) as ex:
+			files = itertools.filterfalse(lambda x:x is None, ex.map(files_in_dir, dirs_to_seek))
+		return list(itertools.chain(*files))
+	except Exception as e:
+		raise e
 
-fseek = fileseeker()
-
-fileiter = fseek.get_all_files("/home/iquant")
-
-print(*fileiter[0])
+#Some testing
+if __name__ == "__main__":
+	print("Starting test of 'get_all_files'")
+	fileiter = get_all_files("/home/")
+	print(*fileiter[:30], sep="\n")
+	print("Found", len(fileiter), "files")
+	print("Ended test of 'get_all_files'")
 

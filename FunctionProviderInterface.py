07b44f2ef8612dcae45203798afe005d21711636
IQuant <quant3234@gmail.com>
2017-11-08 14:52:03 +0300

    Added main.py, updates for FPI.py

--- FunctionProviderInterface.py
+++ FunctionProviderInterface.py
@@ -1,21 +1,39 @@
 import time
 import sys
 from random import randint
+import threading
 
-class GlobalExchangeTable:
-	def __init__(self):
-		self.hashmap = {}
-	def add_interface(self, IFEI):   #FunctionExchangeInterface
-		if name not in self.hasmap:
-			self.hashmap[name] =  IFEI
-			return True
-		else:
-			return False
+
+
+class TaskListExecutor:
+	def __init__(self, tasklist, resultlist, lock, per_run=100):
+		self.tasklist = tasklist
+		self.resultlist = resultlist
+		self.lock = lock
+		self.per_run = per_run
+
+	def thread_target(self):
+		while True:
+			self.lock.acquire()
+			left = min(self.per_run, len(self.tasklist))
+			for key in self.tasklist:
+				if left == 0:
+					break
+				if key in self.resultlist:
+					continue
+				left -= 1
+				task = self.tasklist[key]
+				print("Executed", key)
+				self.resultlist[key] = (task[0](*task[1], **task[2]), True)
+			self.lock.release()
+
+
 
 class FunctionTasker:
 	def __init__(self, tasklist, function):
 		self.tasklist = tasklist
 		self.function = function
+
 	def callable(self, *args, **kwargs):
 		while True:
 			identifier = id(self.function) + (time.time() % 100000) * 1000000 + len(tasklist) + randint(0, 10000000)
@@ -28,42 +46,49 @@ class FunctionTasker:
 		return identifier
 
 
-class FunctionProviderInterface:
-	def __init__(self, name, global_exchange_table):
-		self.my_exchange_interface = global_exchange_table[name]
-		self.name = name
 
 class FunctionUserInterface:
-	def __init__(self, UniversalExchangeData):
-		self.provideble = UniversalExchangeData[0]
-		self.tasklist   = UniversalExchangeData[1]
-		self.resultlist = UniversalExchangeData[2]
+	def __init__(self, ExchangeFingerprint, lock):
+		self.provideble = ExchangeFingerprint[0]
+		self.tasklist   = ExchangeFingerprint[1]
+		self.resultlist = ExchangeFingerprint[2]
+		self.lock		= lock
+
 	def task_function(self, function_name): #TODO: Make Working
 		return  self.providing[function_name]()
+
 	def get_function_result(self, callable_id, pause_time=0.1):
 		while True:
+			lock.acquire()
 			if callable_id in self.resultlist:
-				return self.resultlist[callable_id]
-			timee.sleep(pause_time)
+				mem = self.resultlist[callable_id]
+				self.tasklist[callable_id] = None
+				self.resultlist[callable_id] = None
+				lock.release()
+				return mem[0]	
+			lock.release()		
+			time.sleep(pause_time)
+
+
 
 class FunctionExchangeInterface:
-	def __init__(self, name):
+	def __init__(self, name, global_exchange_table):
 		self.name = name
 		self.providing = {}
 		self.tasklist = {}
 		self.resultlist = {}
+		self.ExchangeFingerprint = (self.providing, self.tasklist, self.resultlist)
+		global_exchange_table[name] = self.ExchangeFingerprint
+		self.lock = threading.lock()
+
 	def add_provideble(self, function_name, function):
 		self.providing[function_name] = FunctionTasker(self.tasklist, function).callable
-
-
 
 
 
 FPI = FunctionProviderInterface
 FUI = FunctionUserInterface
 FEI = FunctionExchangeInterface
-	
-
 
 
 
@@ -72,21 +97,35 @@ if __name__ == "__main__":
 	print("Testing FunctionTasker")
 	
 	tasklist = {}
+	resultlist = {}
 	callables = []
 	n = 100
 	m = 100
 	
 	for i in range(n):
 		callables.append(FunctionTasker(tasklist, print).callable)
+
 	print("Len of callables is", len(callables))
 	print("Testing callables")
+
 	for call in callables:
 		for i in range(m):
-			call()
+			call(i)
+
 	print("Done")
 	print("Found", len(tasklist), "elements in tasklist")
 	
-	if len(tasklist) == n*m:
+	print("Making tasklist executor")
+	lock = threading.Lock()
+	executor = TaskListExecutor(tasklist, resultlist, lock)
+	executor_thread = threading.Thread(target=executor.thread_target, daemon=True)
+	executor_thread.start()
+	time.sleep(1)
+	
+	print("Found", len(tasklist), "elements in tasklist")
+	print("Found", len(resultlist), "elements in resultlist")
+	
+	if len(resultlist) == n*m:
 		print("Passed")
 		time.sleep(1)
 	else:
---
+++ main.py
@@ -0,0 +1,1 @@
+import FunctionProviderInterface as FPI
